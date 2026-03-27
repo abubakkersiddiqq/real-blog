d4318f080b3f65104836415c85438d42c271b740
abubakkersiddiqq <abusiddiq5556@gmail.com>
2026-03-27 22:32:11 +0530

    changed web interface

--- routers/web.py
+++ routers/web.py
@@ -57,3 +57,19 @@ async def user_posts_page(request: Request, user_id: int, db: Annotated[AsyncSes
             "title" : f"{user.username}'s Posts "
         }
     )
+@router.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"title": "Login"},
+    )
+
+
+@router.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"},
+    )
