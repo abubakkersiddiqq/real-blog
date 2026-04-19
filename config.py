a3d58abd16132d16f069a16c6199e961ed8f1936
abubakkersiddiqq <abusiddiq5556@gmail.com>
2026-04-19 16:13:15 +0530

    railway: fixing the static

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file= ".env",
-        env_file_encoding= "utf-8"
+        env_file_encoding= "utf-8",
+        extra="ignore"
     )
     
     database_url: str
